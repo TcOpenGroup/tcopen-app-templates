4263a1374a08dafa1b8abeefeedd4c05f3eab09c
Peter <61538034+PTKu@users.noreply.github.com>
2022-05-17 11:43:54 +0200

    Revert "9 create scaffolding cli for templates (#12)" (#13)
    This reverts commit 81c9bd2ed6a1007392e682cdd73521e912fce57f.

--- build.ps1
+++
@@ -1,4 +0,0 @@
-$root = Get-Location
-Set-Location templates\mts-s-template
-./build.ps1
-Set-Location $root
--- templates/mts-s-template/build.ps1
+++
@@ -1,9 +0,0 @@
-# dotnet run --project cake/Build.csproj -- $args
-# We need to use msbuild instead of dotnet due to COMrefs in the project.
-
-$msbuild =([System.Environment]::GetEnvironmentVariable('TcoMsbuild'))
-cd cake/
-& $msbuild cake/Build.csproj /t:Rebuild /p:Configuration=Release /p:Platform=AnyCPU /p:TargetFramework=net5.0 /p:DefineConstants=Release $args
-.\bin\Release\net5.0\Build.exe $args    
-cd ..
-exit $LASTEXITCODE;
