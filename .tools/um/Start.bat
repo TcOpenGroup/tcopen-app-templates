671a41ac631ee78d707a13044013e56d3c08b18f
Peter <61538034+PTKu@users.noreply.github.com>
2022-05-21 14:32:12 +0200

    um batch file added to .tools folder

---
+++ .tools/um/Start.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+
+set TC_RT_INST_NETID=192.168.4.1.1.1
+set TC_RT_INST_BUILD=4024.22
+
+for %%I in (.) do set TC_INST_NAME=%%~nxI
+%TWINCAT3DIR%Runtimes\bin\Build_%TC_RT_INST_BUILD%\TcSystemServiceUm.exe -i %TC_RT_INST_NETID% -n %TC_INST_NAME% -c ".\3.1"
