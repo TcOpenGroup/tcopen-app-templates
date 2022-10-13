ab11409079e3b855f054b4dacfc49c263049d7cf
david.spavor <david.spavor@mts.sk>
2022-10-13 15:20:36 +0200

    update to latest tcopen packages, added more fixes to task view, added basic authorization

---
+++ templates/mts-s-template/t/src/x_template_xHmi.Blazor/Pages/ControlledUnits/Base/ControlledUnitBase/Tasks/TcoTaskedService/Task/TcoTaskedServiceTaskCommandViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vortex.Presentation;
+
+namespace x_template_xPlc
+{
+    public class TcoTaskedServiceTaskCommandViewModel : RenderableViewModelBase
+    {
+        public TcoTaskedServiceTaskCommandViewModel()
+        {
+
+        }
+
+        TcoTaskedService component;
+        public TcoTaskedService Component
+        {
+            get => component;
+            private set
+            {
+                if (component == value)
+                {
+                    return;
+                }
+
+                SetProperty(ref component, value);
+            }
+        }
+
+        public override object Model { get => Component; set => Component = (TcoTaskedService)value; }
+    }
+}
