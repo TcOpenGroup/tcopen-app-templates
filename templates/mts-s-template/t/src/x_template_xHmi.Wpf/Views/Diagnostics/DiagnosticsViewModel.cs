dfa6cfbca2954b0ffc8b4ee5223d35b67fdcf13d
Peter Barancek <peter.barancek@mts.sk>
2023-03-27 15:28:29 +0200

    *package updated +general diagnostics tab +TopologyRenderer for Ecat diagnostic as modal window +IO/TreeView of HW struct * visual colors  in controled unit * disable console subwindow->add splash screen

---
+++ templates/mts-s-template/t/src/x_template_xHmi.Wpf/Views/Diagnostics/DiagnosticsViewModel.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using TcoCore;
+using TcoIo;
+using TcOpen.Inxton;
+using TcOpen.Inxton.Input;
+using x_template_xPlc;
+
+namespace x_template_xHmi.Wpf.Views.Diagnostics
+{
+    public class DiagnosticsViewModel : MenuControlViewModel
+    {
+        public DiagnosticsViewModel() : base()
+        {
+
+            ShowTopologyCommand = new RelayCommand(a => ShowTopology());
+        }
+
+        private void ShowTopology()
+        {
+            TcoAppDomain.Current.Dispatcher.Invoke(
+           () =>
+           {
+               var win = new TopologyView();
+               var viewInstance = Activator.CreateInstance(win.GetType());
+        
+                win = viewInstance as TopologyView;
+               if (win != null)
+               {
+                   win.DataContext = x_template_xPlc.GVL_iXlinker;
+                   win.Show();
+
+                  
+               }
+           }
+           );
+        }
+        public TopologyRenderer TopologyView { get; } = new TopologyRenderer();
+        public x_template_xPlcTwinController x_template_xPlc { get { return App.x_template_xPlc; } }
+
+        public RelayCommand ShowTopologyCommand { get; }
+    }
+}
